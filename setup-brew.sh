4e1ea6fa0b909065b33bb1d29e70d7d9bd7c0717
Wojciech Piekutowski <wojciech@piekutowski.net>
2025-10-14 11:10:01 +0200

    Update Aerospace setup and add brew setup script

---
+++ setup-brew.sh
@@ -0,0 +1,64 @@
+# Homebrew
+xcode-select --install
+
+if [[ ! -f "/opt/homebrew/bin/brew" ]]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+brew update && brew upgrade
+
+brew install --no-quarantine mhaeuser/mhaeuser/battery-toolkit
+brew install sst/tap/opencode
+
+brew install nikitabobko/tap/aerospace
+brew install FelixKratz/formulae/borders
+brew install mediosz/tap/swipeaerospace
+
+# objective-see
+brew install \
+	blockblock \
+	reikey \
+	ransomwhere \
+	knockknock \
+	taskexplorer \
+	netiquette \
+	oversight \
+
+# Colima provides VM for docker
+brew install \
+        asdf \
+        betterdisplay \
+	chatgpt \
+	codex \
+        colima \
+	devcontainer \
+        difftastic \
+        direnv \
+        docker \
+        docker-buildx \
+        docker-compose \
+        docker-credential-helper \
+        entr \
+        iterm2 \
+	itermai \
+        jq \
+        mise \
+        neovim \
+        nodejs \
+        tmux \
+        ungoogled-chromium \
+	uv \
+        visual-studio-code \
+        wget \
+	# YubiKey manager
+	ykman \
+        yq \
+	zsh \
+
+	# It seems autoupgrade doesn't work when installed via homebrew 
+        # google-chrome \
+	# zed@preview \
+	
+	# Stores credentials in easily accessible way in keychain :(
+	# gh \
+
